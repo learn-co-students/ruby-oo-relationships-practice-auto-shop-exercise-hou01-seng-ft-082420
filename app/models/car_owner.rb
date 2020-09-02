50a194237ee5208f768d1411c4d6bfd80e8ed257
paytongray18 <paytongray18@gmail.com>
2020-09-01 23:44:19 -0500

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,32 @@
 class CarOwner
 
   attr_reader :name
+  attr_accessor :cars, :mechanic
 
+  @@all = []
   def initialize(name)
     @name = name
+
+    self.class.all << self
   end
 
+  def cars
+    Car.all.select do |car|
+      car.owner == self
+    end
+  end
+
+  def mechanics
+    self.cars.collect{|c| c.mechanic}
+  end
+
+  def self.average_number_of_cars
+    total = Car.all.count
+    total / self.all.count
+  end
+
+  def self.all
+    @@all
+  end
 
 end
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -2,10 +2,31 @@ class Mechanic
 
   attr_reader :name, :specialty
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  @@all = []
+
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def cars
+    Car.all.select {|c| c.mechanic == self}
+  end
+
+  def car_owners
+    self.cars.collect{|co|co.owner}
+  end
+
+  def car_owners_names
+    self.car_owners.collect{|con|con.name}
+  end
+  
 
 
 end
